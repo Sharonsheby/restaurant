6ba5f4f8bfac836ab1e6987841d815a9a6e215c8
unknown <sharonsheby96@gmail.com>
2023-04-16 18:16:09 +0530

    redux added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './components/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Provider>
+    <App store={store} />
+    </Provider>
+    
     </BrowserRouter>
     
   </React.StrictMode>
